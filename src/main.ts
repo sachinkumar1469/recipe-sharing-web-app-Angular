c9df3a199831fc2f12c44c9905c3f00ee50bbc19
sachinkumar1469 <sachinyadav1469@gmail.com>
2023-02-22 22:15:30 +0530

    Setting Up Angular Project

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
-console.log("Here in main.ts");
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
